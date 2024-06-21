b108d583d13e5860a98169af293451b2e3701d34
lakko <lakko@LAPTOP-TSGU3JAG.attlocal.net>
2024-06-21 19:31:22 -0400

    updated and triggered the poll scm

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -4,6 +4,6 @@ public class MyFirstClass {
 	public static void main(String[]args) {
 		
 		System.out.println("This is my first program");
-		System.out.println("i like doing the technical sesssions");
+		System.out.println("i like doing the technical sessions");
 	}
 }
