90e8fb268cdf835d47b70b4c969647298816aaeb
lakko <lakko@LAPTOP-TSGU3JAG.attlocal.net>
2024-06-21 19:38:38 -0400

    updated the poll scm trigger

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -5,5 +5,6 @@ public class MyFirstClass {
 		
 		System.out.println("This is my first program");
 		System.out.println("i like doing the technical sessions");
+		System.out.println("i now triggered the poll scm and practiced it");
 	}
 }
