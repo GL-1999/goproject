d8643822b164899d96d4f9711671c0e55dc3da5a
lakko <lakko@LAPTOP-TSGU3JAG.attlocal.net>
2024-06-21 19:30:45 -0400

    updated and commited for poll scm

--- src/MyFirstClass.java
+++ src/MyFirstClass.java
@@ -4,5 +4,6 @@ public class MyFirstClass {
 	public static void main(String[]args) {
 		
 		System.out.println("This is my first program");
+		System.out.println("i like doing the technical sesssions");
 	}
 }
